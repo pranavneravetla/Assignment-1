01f169a34e03dd04954141b0de864e4536c7513b
pranavneravetla <pranavneravetla@gmail.com>
2016-07-03 17:51:55 +0530

    Add files via upload

---
+++ Integer.java
@@ -0,0 +1,28 @@
+
+public class Integer {
+
+	public static void main(String[] args) {
+		int number;
+		int num1=22;
+		int num4=31;
+		number=2;
+		int product= num1 * num4;
+		int addition= num4 - num1;  
+		int division= num1 / number;
+		int exponent= number + number;
+		
+		
+		System.out.println(product);
+		System.out.println(addition);
+		System.out.println(division);
+		System.out.print(exponent);
+				
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
